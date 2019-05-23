cffa2ef35ac27ec22ec45a070d295ffd6dd16000
-L0Lock- <16049822+L0Lock@users.noreply.github.com>
2019-05-23 18:45:03 +0200

    correct bug with file names containing spaces

--- dragNdrop_Discord.cmd
+++ dragNdrop_Discord.cmd
@@ -1,7 +1,7 @@
 @echo off
 cd /D %~p0
 SET output=%~n1_8MB.mp4
-set cmd="ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "%1" "
+set cmd="ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 %1 "
 FOR /F "tokens=*" %%i IN (' %cmd% ') DO SET seconds=%%i
 echo aaaa
 echo aaaa
@@ -10,6 +10,6 @@ SET overheadBitrate=100
 SET audioBitrate=96
 SET /A "videoBitrate=totalBitrate-audioBitrate-overheadBitrate"
 ffmpeg -y -i %1 -c:v libx264 -b:v %videoBitrate%k -pass 1 -b:a %audioBitrate%k -f mp4 NUL && \
-ffmpeg -i %1 -c:v libx264 -b:v %videoBitrate%k -pass 2 -b:a %audioBitrate%k "%output%"
+ffmpeg -y -i %1 -c:v libx264 -b:v %videoBitrate%k -pass 2 -b:a %audioBitrate%k "%output%"
 del /q ffmpeg2pass-*.log ffmpeg2pass-*.mbtree
 if NOT ["%errorlevel%"]==["0"] pause

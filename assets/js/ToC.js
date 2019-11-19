61fdf7bcc411b42e1270a9d39a6100a1efd87c00
-L0Lock- <myuline.annatar@gmail.com>
2019-11-19 22:26:05 +0100

    js fix ++++

--- assets/js/ToC.js
+++ assets/js/ToC.js
@@ -1,29 +1,32 @@
-var ToC =
-  "<nav role='navigation' class='table-of-contents'>" +
-    "<h2>Jump to file description:</h2>" +
-    "<ul>";
+$(document).ready(function() {
+  var ToC =
+    "<nav role='navigation' class='table-of-contents'>" +
+      "<h2>Jump to file description:</h2>" +
+      "<ul>";
 
-var newLine, el, title, link;
+  var newLine, el, title, link;
 
-$("main h4").each(function() {
+  $("main h4").each(function() {
 
-  el = $(this);
-  title = el.text();
-  link = "#" + el.attr("id");
+    el = $(this);
+    title = el.text();
+    link = "#" + el.attr("id");
 
-  newLine =
-    "<li>" +
-      "<a href='" + link + "'>" +
-        title +
-      "</a>" +
-    "</li>";
+    newLine =
+      "<li>" +
+        "<a href='" + link + "'>" +
+          title +
+        "</a>" +
+      "</li>";
 
-  ToC += newLine;
+    ToC += newLine;
+
+  });
+
+  ToC +=
+     "</ul>" +
+    "</nav>";
+
+  $(".file-descriptions").prepend(ToC);
 
 });
-
-ToC +=
-   "</ul>" +
-  "</nav>";
-
-$(".file-descriptions").prepend(ToC);

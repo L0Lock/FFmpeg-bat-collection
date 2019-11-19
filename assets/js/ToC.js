86d59c9ce9a5893b7b80152c21c08e0db5f69395
-L0Lock- <myuline.annatar@gmail.com>
2019-11-19 22:03:45 +0100

    fix JS & Jquerry injection

---
+++ assets/js/ToC.js
@@ -0,0 +1,29 @@
+var ToC =
+  "<nav role='navigation' class='table-of-contents'>" +
+    "<h2>Jump to file description:</h2>" +
+    "<ul>";
+
+var newLine, el, title, link;
+
+$("main h4").each(function() {
+
+  el = $(this);
+  title = el.text();
+  link = "#" + el.attr("id");
+
+  newLine =
+    "<li>" +
+      "<a href='" + link + "'>" +
+        title +
+      "</a>" +
+    "</li>";
+
+  ToC += newLine;
+
+});
+
+ToC +=
+   "</ul>" +
+  "</nav>";
+
+$(".file-descriptions").prepend(ToC);

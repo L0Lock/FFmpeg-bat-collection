596d9b15fc2a4dc6289ff2ded70aa29cc93a8932
-L0Lock- <myuline.annatar@gmail.com>
2020-02-03 16:03:33 +0100

    update GIF procedure so it deletes the generated palette file at the job's end

--- dragNdrop_GIF_1fps.bat
+++ dragNdrop_GIF_1fps.bat
@@ -1,10 +1,10 @@
 echo off
 :again
 
-ffmpeg.exe -i "%~1" -y -filter_complex "[0:v] palettegen" palette.png
 mkdir frames
+ffmpeg.exe -i "%~1" -y -filter_complex "[0:v] palettegen" "frames/palette.png"
 ffmpeg -i "%~1" "frames/ffout%%03d.png"
-ffmpeg.exe -framerate 1 -i "frames/ffout%%03d.png" -i palette.png -filter_complex "[0:v][1:v] paletteuse" -r 1 "%~p1%~n1.gif"
+ffmpeg.exe -framerate 1 -i "frames/ffout%%03d.png" -i "frames/palette.png" -filter_complex "[0:v][1:v] paletteuse" -r 1 "%~p1%~n1.gif"
 rmdir frames /s /q
 shift
 if "%~1" == "" goto:eof

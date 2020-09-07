03dda2713e5b8fe166a7db9a1e6abf56893313c6
-L0Lock- <myuline.annatar@gmail.com>
2020-09-07 18:40:56 +0200

    replaced Video_ADD_Cover.bat with two scripts for MP4 and MKV individually

---
+++ ADD_Cover_to_MKV.bat
@@ -0,0 +1,30 @@
+
+:again
+
+cd /D %~p1
+
+ffmpeg ^
+	-i "%~nx1" ^
+	-attach "cover.jpg" ^
+	-metadata:s:t mimetype=image/jpeg ^
+	-c copy ^
+	"%~p1%~n1_covered.mkv"
+	pause
+if NOT ["%errorlevel%"]==["0"] goto:error
+echo [92m%~n1 Done![0m
+
+shift
+if "%~1" == "" goto:end
+goto:again
+
+:error
+
+echo [93mThere was an error. Please check your input file or report an issue on github.com/L0Lock/FFmpeg-bat-collection/issues.[0m
+pause
+exit 0
+
+:end
+
+cls
+echo [92mEncoding succesful. This window will close after 10 seconds.[0m
+timeout /t 10
---
+++ ADD_Cover_to_MP4.bat
@@ -0,0 +1,32 @@
+
+:again
+
+cd /D %~p1
+
+ffmpeg ^
+	-i "%~nx1" ^
+	-i "cover.jpg" ^
+	-map 0 -map 1 ^
+	-c copy ^
+	-c:v:1 png ^
+	-disposition:v:1 attached_pic ^
+	"%~p1%~n1_covered.mp4"
+	pause
+if NOT ["%errorlevel%"]==["0"] goto:error
+echo [92m%~n1 Done![0m
+
+shift
+if "%~1" == "" goto:end
+goto:again
+
+:error
+
+echo [93mThere was an error. Please check your input file or report an issue on github.com/L0Lock/FFmpeg-bat-collection/issues.[0m
+pause
+exit 0
+
+:end
+
+cls
+echo [92mEncoding succesful. This window will close after 10 seconds.[0m
+timeout /t 10
